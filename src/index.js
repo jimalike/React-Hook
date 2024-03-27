6be3e4bf368e552b3a72a7d39b3692b3272f5146
Supanut <jimsupanut@gmail.com>
2024-03-27 23:43:49 +0700

    css

--- src/index.js
+++ src/index.js
@@ -2,11 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
+function App() {
+  return (
+    <div className='App'>
+      <div className='header'>
+        <h1>Movie App</h1>
+      </div>
+      <div className='search'>
+        <input type='text' placeholder='keyword ?'></input>
+        <button>search</button>
+      </div>
+
+      <div className='movie-lists'>
+        <div className='movie'>Movie-1</div>
+        <div className='movie'>Movie-2</div>
+      </div>
+    </div>
+
+  )
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <h1>
-      React Hook
-    </h1>
-
+  <App />
 );
 
